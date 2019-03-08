9d37e636616534d711dcfa9003ee7853bf958c6c
ArcEcho <zmy_qwerty@163.com>
2019-03-08 14:05:15 +0800

    Fix setup script.

--- setup_blog.bat
+++ setup_blog.bat
@@ -1,6 +1,10 @@
 @echo off
-npm install hexo-cli --save
-npm install hexo-deployer-git --save
-npm install hexo-asset-image --save
-npm audit fix
-npm install
+call npm install hexo  --save  
+call npm install hexo-cli --save
+call npm install hexo-deployer-git --save
+call npm install hexo-fs --save
+call npm install hexo-asset-image --save
+call npm install hexo-server --save
+call  npm audit fix
+call npm install
+
